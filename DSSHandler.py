d9f03dfa8183db0777767902d8892addf2a95b55
Grant Hendrickson <granthendrickson19@gmail.com>
2025-02-03 16:24:17 -0800

    using genfromtxt within numpy to read my csv data file

--- DSSHandler.py
+++ DSSHandler.py
@@ -2,14 +2,24 @@ import numpy as np
 import sys
 import argparse
 import yaml
+from numpy import genfromtxt
+
+
 
 parser  = argparse.ArgumentParser(
     description="This command line program will take an input CSV file and apply DSS analysis "
     )
 parser.add_argument('-d','--data',required=True, help='Data file to analyze')
-parser.add_argument('-m','--model',required=True, help='Model YAML file for data predicition')
-parser.add_argument('-o','--output',required=True, help='Output file name')
+#parser.add_argument('-m','--model',required=True, help='Model YAML file for data predicition')
+#parser.add_argument('-o','--output',required=True, help='Output file name')
+#I want to have three inputs on my command line 
+
 
 #adding this comment
 
 args = parser.parse_args(sys.argv[1:])
+#print (args.data)
+#print (args.model)
+
+my_data = genfromtxt(args.data, delimiter =',',skip_header=1)
+print(my_data)
