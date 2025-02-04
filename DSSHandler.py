9db6bc1c34d9cb8ec298d5f6f8e4897aa3639ef9
Grant Hendrickson <granthendrickson19@gmail.com>
2025-02-03 17:36:55 -0800

    added a YAML input file that is a dictionary of inputs

--- DSSHandler.py
+++ DSSHandler.py
@@ -10,7 +10,7 @@ parser  = argparse.ArgumentParser(
     description="This command line program will take an input CSV file and apply DSS analysis "
     )
 parser.add_argument('-d','--data',required=True, help='Data file to analyze')
-#parser.add_argument('-m','--model',required=True, help='Model YAML file for data predicition')
+parser.add_argument('-m','--model',required=True, help='Model YAML file for data predicition')
 #parser.add_argument('-o','--output',required=True, help='Output file name')
 #I want to have three inputs on my command line 
 
@@ -21,5 +21,10 @@ args = parser.parse_args(sys.argv[1:])
 #print (args.data)
 #print (args.model)
 
+#data should be in format of time | conserveed quantity of interest
 my_data = genfromtxt(args.data, delimiter =',',skip_header=1)
-print(my_data)
+#print(my_data)
+
+with open(args.model,'r') as f:
+    inputs = yaml.safe_load(f)
+#print(inputs['normalize'])
