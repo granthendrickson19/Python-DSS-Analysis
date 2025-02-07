e8ebb74406fd74c579000c30cfda5711cc81bd8f
Grant Hendrickson <granthendrickson19@gmail.com>
2025-02-07 14:13:50 -0800

    Reinstalled Sphinx, successfully added first module to documentation

--- DSSHandler.py
+++ DSSHandler.py
@@ -5,26 +5,45 @@ import yaml
 from numpy import genfromtxt
 
 
-
-parser  = argparse.ArgumentParser(
-    description="This command line program will take an input CSV file and apply DSS analysis "
-    )
-parser.add_argument('-d','--data',required=True, help='Data file to analyze')
-parser.add_argument('-m','--model',required=True, help='Model YAML file for data predicition')
+if __name__ == "__main__":
+    parser  = argparse.ArgumentParser(
+        description="This command line program will take an input CSV file and apply DSS analysis "
+        )
+    parser.add_argument('-d','--data',required=True, help='Data file to analyze')
+    parser.add_argument('-m','--model',required=True, help='Model YAML file for data predicition')
+    args = parser.parse_args(sys.argv[1:])
+    my_data = genfromtxt(args.data, delimiter =',',skip_header=1)
+    with open(args.model,'r') as f:
+        inputs = yaml.safe_load(f)
+    
 #parser.add_argument('-o','--output',required=True, help='Output file name')
 #I want to have three inputs on my command line 
 
 
 #adding this comment
 
-args = parser.parse_args(sys.argv[1:])
+
 #print (args.data)
 #print (args.model)
 
 #data should be in format of time | conserveed quantity of interest
-my_data = genfromtxt(args.data, delimiter =',',skip_header=1)
+
 #print(my_data)
 
-with open(args.model,'r') as f:
-    inputs = yaml.safe_load(f)
-#print(inputs['normalize'])
+
+
+def data_normalizer(data, inputs):
+    """Normalizes Data to Users Input, Creates conserved quantity for DSS analysis
+
+    Parameters
+    ----------
+    data : ndarray
+        Data set to be normalized
+    inputs : dictionary
+        Input file with corresponding model and data settings
+    
+    Returns
+    -------
+    ndarray
+        Array with normalized data (array of Beta's in DSS terminology)
+    """
