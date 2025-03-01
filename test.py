be14d4ae3ee70eb0c5e64a07de48f564ca16da29
Grant Hendrickson <granthendrickson19@gmail.com>
2025-02-28 16:23:57 -0800

    began to add more tests, figured out correct assert statement

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pytest
 import DSSHandler as DSS    
 
 def test_data_normalzier_first():
@@ -18,7 +17,7 @@ def test_data_normalzier_last():
     data = np.array([1,2,3,4,5])
     obs = DSS.data_normalizer(data, quad_dict)
     exp = np.array([0.2,.4,.6,.8,1])
-    assert exp.all() == obs.all()
+    assert np.allclose(exp,obs)
 
 def test_time_derivative():
     #Trivial Solution no change with time
@@ -26,7 +25,7 @@ def test_time_derivative():
     time =np.array([1,2,3])
     exp = np.array([0,0,0])
     obs = DSS.time_derivative(data,time)
-    assert exp.all() == obs.all()
+    assert np.allclose(exp,obs)
 
 def test_time_derivative_linear():
     #Linear time change
@@ -34,7 +33,8 @@ def test_time_derivative_linear():
     time = np.array([1,2,3])
     exp = np.array([1,1,1])
     obs = DSS.time_derivative(data,time)
-    assert exp.all() == obs.all()
+    assert np.allclose(exp,obs)
+
 def test_model_data_generator_quad():
     quad_dict = {
         "A" : "1",
@@ -45,4 +45,16 @@ def test_model_data_generator_quad():
     time = np.array([0,1,2])
     obs = DSS.model_data_generator(time,quad_dict)
     exp = np.array([1,3,7])
-    assert exp.all() == obs.all()
+    assert np.allclose(exp,obs)
+
+def test_model_data_generator_exp():
+    quad_dict = {
+        "A" : "1",
+        "B" : "1",
+        "C" : "1",
+        "model":"exp"
+    }
+    time = np.array([-100,0,1])
+    obs = DSS.model_data_generator(time,quad_dict)
+    exp = np.array([1,2,3.71828])
+    assert np.allclose(exp,obs)
